bd04501e1f54054ab57cf36bc4d6178757172b13
이동민 <lee45620785@gmail.com>
2022-08-02 18:04:57 +0900

    sol

---
+++ src/com/bj/Main_BJ_S2_10997_별찍기22.java
@@ -0,0 +1,20 @@
+package com.bj;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+public class Main_BJ_S2_10997_별찍기22 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		String[][] stars = new String[n*4-1][];
+		
+		System.out.println(Arrays.toString(stars));
+
+	}
+}
