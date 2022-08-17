c414fb581557080f93bf7166c02c78e1a42086f3
이동민 <fftl0785@gmail.com>
2022-08-17 23:37:22 +0900

    update 0817

--- src/com/bj/Main_BJ_11000_G5_강의실배정.java
+++ src/com/bj/Main_BJ_11000_G5_강의실배정.java
@@ -3,23 +3,34 @@ package com.bj;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main_BJ_11000_G5_강의실배정 {
 
-	static ArrayList<int[]> list;
-	
-	//우선순위큐로 종료, 시작  오름차순 정렬
-	//가장 처음꺼를 뺸뒤, 하나의 강의실에서 이어질 수 있는 강의를 모두 찾아냅니다.
-	//그리고 그 다음 강의실 +++ 반복
+	static boolean check(boolean[] visited) {
+		boolean run = false;
+		for (int i = 0; i < visited.length; i++) {
+			if (!visited[i]) {
+				run = true;
+				break;
+			}
+		}
+		return run;
+	}
+
+	// 우선순위큐로 종료, 시작 오름차순 정렬
+	// 가장 처음꺼를 뺸뒤, 하나의 강의실에서 이어질 수 있는 강의를 모두 찾아냅니다.
+	// 그리고 그 다음 강의실 +++ 반복
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
-		list = new ArrayList<>();
+		ArrayList<int[]> list = new ArrayList<>();
+		boolean[] visited = new boolean[N];
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -38,9 +49,27 @@ public class Main_BJ_11000_G5_강의실배정 {
 				}
 			}
 		});
+
+		int cnt = 0;
+		while (check(visited)) {
+			cnt++;
+			for(int i=0; i<list.size(); i++) {
+				if(!visited[i]) {
+					int end = list.get(i)[1];
+					visited[i] = true;
+					
+					for (int j = i+1; j < list.size(); j++) {
+						if(!visited[j] && end<=list.get(j)[0]) {
+							visited[j] = true;
+							end = list.get(j)[1];
+						}
+					}
+					
+					break;
+				}
+			}
+		}
 		
-		System.out.println();
-		
-		
+		System.out.println(cnt);
 	}
 }
