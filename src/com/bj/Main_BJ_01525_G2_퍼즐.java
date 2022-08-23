d1976a2106867c21a00b059a242499d9a5daf462
이동민 <fftl0785@gmail.com>
2022-08-24 01:23:30 +0900

    update 220823

---
+++ src/com/bj/Main_BJ_01525_G2_퍼즐.java
@@ -0,0 +1,32 @@
+package com.bj;
+
+import java.util.Scanner;
+
+public class Main_BJ_01525_G2_퍼즐 {
+
+	public static void main(String[] args) throws Exception{
+
+		Scanner sc = new Scanner(System.in);
+		int[][] map = new int[3][3];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+}
