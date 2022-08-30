2b6b0407083632e39580d6672619577962b6ee34
이동민 <fftl0785@gmail.com>
2022-08-30 17:35:20 +0900

    update 220830

--- src/com/zother/Dijkstra.java
+++ src/com/zother/Dijkstra.java
@@ -71,57 +71,57 @@ public class Dijkstra {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(in.readLine().trim());
-		int V = Integer.parseInt(st.nextToken()); //정점 갯수
+		int V = Integer.parseInt(st.nextToken()); // 정점 갯수
 		int start = 0;
-		int end =  V-1; //도착점 인덱스
+		int end = V - 1; // 도착점 인덱스
 		final int INFINITY = Integer.MAX_VALUE;
-		
+
 		int[][] matrix = new int[V][V];
 		int[] distance = new int[V];
 		boolean[] visited = new boolean[V];
-		
-		for(int i=0; i<V; ++i){
+
+		for (int i = 0; i < V; ++i) {
 			st = new StringTokenizer(in.readLine().trim(), " ");
-			for(int j=0; j<V; ++j){
+			for (int j = 0; j < V; ++j) {
 				matrix[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+
 		Arrays.fill(distance, INFINITY);
 		distance[start] = 0;
-		
-		int min=0, current=0;
-		for(int i=0; i<V; ++i){
-			//1단계 : 방문하지 않은 정점들 중 최소가중치의 정점 선택
-			min = INFINITY;  //최소 비용의 방문하지 않은 노드 찾기 위한 초기화
-			for(int j=0; j<V; ++j){
-				if(!visited[j]     
-					//방문하지 않았고(아직 출발점 또는 출발점으로 부터의 선택된 기준이 되지 못했다면) 
-					&& distance[j] < min){  //최소 비용을 갖는 노드가 있다면
-					min = distance[j];  //최소 비용값 저장
-					current = j;        //가장 최소 비용을 갖는 노드의 위치 확인
+
+		int min = 0, current = 0;
+		for (int i = 0; i < V; ++i) {
+			// 1단계 : 방문하지 않은 정점들 중 최소가중치의 정점 선택
+			min = INFINITY; // 최소 비용의 방문하지 않은 노드 찾기 위한 초기화
+			for (int j = 0; j < V; ++j) {
+				if (!visited[j]
+						// 방문하지 않았고(아직 출발점 또는 출발점으로 부터의 선택된 기준이 되지 못했다면)
+						&& distance[j] < min) { // 최소 비용을 갖는 노드가 있다면
+					min = distance[j]; // 최소 비용값 저장
+					current = j; // 가장 최소 비용을 갖는 노드의 위치 확인
 				}
 			}
 			visited[current] = true; // 선택 정점 방문 처리
-			if(current == end){ // 선택 정점이 도착정점이면 탈출.
+			if (current == end) { // 선택 정점이 도착정점이면 탈출.
 				break;
 			}
-			
-			//2단계: current정점을 경유지로 하여 갈수 있는 다른 방문하지 않은 정점들에 대한 처리
-			for(int c=0; c<V; ++c){
-				if(!visited[c] //방문하지 않았고 ==> 다시 되돌아 가지 않을래
-						       //이미 최단 거리를 구했고 되돌아 가면 어차피 거리만 늘어나
-				   && 	matrix[current][c] != 0  //진입이 가능한 경로라면
-				   &&  distance[c] > min+matrix[current][c]){ 
-		    //출발점에서 얻은 거리(내거리)와  새로 진행을 통해 얻은 거리(너거리)를 더한 값이 
-			//기존에 저장된 거리보다 최단 거리라면
-					distance[c] = min+matrix[current][c];
-					//최단거리 갱신
+
+			// 2단계: current정점을 경유지로 하여 갈수 있는 다른 방문하지 않은 정점들에 대한 처리
+			for (int c = 0; c < V; ++c) {
+				if (!visited[c] // 방문하지 않았고 ==> 다시 되돌아 가지 않을래
+								// 이미 최단 거리를 구했고 되돌아 가면 어차피 거리만 늘어나
+						&& matrix[current][c] != 0 // 진입이 가능한 경로라면
+						&& distance[c] > min + matrix[current][c]) {
+					// 출발점에서 얻은 거리(내거리)와 새로 진행을 통해 얻은 거리(너거리)를 더한 값이
+					// 기존에 저장된 거리보다 최단 거리라면
+					distance[c] = min + matrix[current][c];
+					// 최단거리 갱신
 				}
 			}
 		}
-		System.out.println(distance[end]); //시작점에서 도착점까지의 최단 비용 출력
-		
+		System.out.println(distance[end]); // 시작점에서 도착점까지의 최단 비용 출력
+
 	}
 
 }
---
+++ src/com/zother/mock/Solution_SW_D0_00000_헌터.java
@@ -0,0 +1,24 @@
+package com.zother.mock;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_SW_D0_00000_헌터 {
+
+	static int N;
+	
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/sw_input_헌터.txt")); // input 가져오기
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+		}
+	}
+
+}
