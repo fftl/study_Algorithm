dc54084bca661838ff0b0c29ff4b6d1595061c27
함정빈 <shadberry02@naver.com>
2022-09-02 18:03:09 +0900

    update 220902

---
+++ src/com/pg/Solution_PG_L0.java
@@ -0,0 +1,13 @@
+package com.pg;
+
+public class Solution_PG_L0 {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(solution());
+	}
+
+	static int solution() {
+		int answer = 0;
+		return answer;
+	}
+}
