09eb0ed8e8221a7f840e92e5195de2b0ef746d34
손유진 <lyl0896@naver.com>
2022-10-07 17:45:19 +0900

    update 221007

---
+++ src/com/bj/Main_BJ_23290_G1_마법사상어와복제.java
@@ -0,0 +1,20 @@
+package com.bj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main_BJ_23290_G1_마법사상어와복제 {
+
+	public static void main(String[] args) throws Exception{
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(br.readLine());
+	}
+}
