7f68cc78a0889cec8a5712f6327ac31ea640883d
이동민 <fftl0785@gmail.com>
2022-11-29 13:59:49 +0900

    update: 1018_체스판 다시 칠하기

---
+++ src/com/bj/bronze/Main_BJ_01085_B3_직사각형에서탈출.java
@@ -0,0 +1,27 @@
+package com.bj.bronze;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_BJ_01085_B3_직사각형에서탈출 {
+
+	public static void main(String[] args) throws Exception{
+
+		Scanner sc = new Scanner(System.in);
+		
+		int[] xy = new int[2];
+		int[] wh = new int[2];
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		
+		System.out.println(Arrays.toString(xy));
+		System.out.println(Arrays.toString(wh));
+		
+		int result = 0;
+		
+		
+		sc.close();
+	}
+}
