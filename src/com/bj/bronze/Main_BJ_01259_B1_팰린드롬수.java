3057b1d4afc35b3bc56a05cc93a4a0470b5ff55a
이동민 <fftl0785@gmail.com>
2022-11-29 14:51:12 +0900

    update: 1259_팰린드롬수

--- src/com/bj/bronze/Main_BJ_01259_B1_팰린드롬수.java
+++ src/com/bj/bronze/Main_BJ_01259_B1_팰린드롬수.java
@@ -1,20 +1,42 @@
 package com.bj.bronze;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main_BJ_01259_B1_팰린드롬수 {
 
 	public static void main(String[] args) throws Exception{
 
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.close();
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int t = Integer.parseInt(br.readLine());
+		while(true) {
+			int n = sc.nextInt();
+			if(n == 0) break;
+			
+			char[] arr = Integer.toString(n).toCharArray();
+			
+			//짝수라면
+			if(arr.length%2==0) {
+				boolean check = true;
+				for (int j = 0; j < arr.length/2; j++) {
+					if(arr[j] != arr[arr.length-1-j]) {
+						check = false;
+						break;
+					}
+				}
+				if(check) System.out.println("yes");
+				else System.out.println("no");
+			} else {
+				boolean check = true;
+				for (int j = 0; j < arr.length/2; j++) {
+					if(arr[j] != arr[arr.length-1-j]) {
+						check = false;
+						break;
+					}
+				}
+				if(check) System.out.println("yes");
+				else System.out.println("no");
+			}
+		}
+		sc.close();
 	}
 }
