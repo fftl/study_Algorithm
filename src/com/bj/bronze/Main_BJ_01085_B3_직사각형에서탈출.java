4f4b51e601d8c24545724117fc6ba2c7576d25d4
이동민 <fftl0785@gmail.com>
2022-11-29 14:06:51 +0900

    update: 1085_직사각형에서 탈출

--- src/com/bj/bronze/Main_BJ_01085_B3_직사각형에서탈출.java
+++ src/com/bj/bronze/Main_BJ_01085_B3_직사각형에서탈출.java
@@ -1,6 +1,5 @@
 package com.bj.bronze;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main_BJ_01085_B3_직사각형에서탈출 {
@@ -9,19 +8,19 @@ public class Main_BJ_01085_B3_직사각형에서탈출 {
 
 		Scanner sc = new Scanner(System.in);
 		
-		int[] xy = new int[2];
-		int[] wh = new int[2];
 		int x = sc.nextInt();
 		int y = sc.nextInt();
 		int w = sc.nextInt();
 		int h = sc.nextInt();
 		
-		System.out.println(Arrays.toString(xy));
-		System.out.println(Arrays.toString(wh));
+		int result = 1001;
+	
+		result = Math.min(result, x-0);
+		result = Math.min(result, w-x);
+		result = Math.min(result, y-0);
+		result = Math.min(result, h-y);
 		
-		int result = 0;
-		
-		
+		System.out.println(result);
 		sc.close();
 	}
 }
