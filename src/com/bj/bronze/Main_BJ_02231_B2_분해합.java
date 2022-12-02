19006068edb71a711d686d59249ee22223751425
이동민 <fftl0785@gmail.com>
2022-12-02 13:47:54 +0900

    update: 10866_덱

---
+++ src/com/bj/bronze/Main_BJ_02231_B2_분해합.java
@@ -0,0 +1,13 @@
+package com.bj.bronze;
+
+import java.util.Scanner;
+
+public class Main_BJ_02231_B2_분해합 {
+
+	public static void main(String[] args) throws Exception{
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+	}
+}
