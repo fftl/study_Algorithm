0fb52aded0f444463a8eec1729a5bc1b0035d3fe
이동민 <fftl0785@gmail.com>
2022-12-04 10:26:35 +0900

    update: 1003_피보나치 함수

--- src/com/baekjoon/silver/Main_BJ_01003_S3_피보나치함수.java
+++ src/com/baekjoon/silver/Main_BJ_01003_S3_피보나치함수.java
@@ -12,25 +12,20 @@ public class Main_BJ_01003_S3_피보나치함수 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		for (int i = 0; i < N; i++) {
-			zero = 0;
-			one = 0;
-			int k = Integer.parseInt(br.readLine());
-			
-			fibo(k);
-			System.out.println(zero+" "+one);
+		int[][] dp = new int[41][2];
+		dp[0][0] = 1;
+		dp[0][1] = 0;
+		dp[1][0] = 0;
+		dp[1][1] = 1;
+		
+		for (int i = 2; i < 41; i++) {
+			dp[i][0] = dp[i-1][0] + dp[i-2][0];
+			dp[i][1] = dp[i-1][1] + dp[i-2][1];
 		}
-	}
-	
-	static int fibo(int k) {
-		if(k == 0) {
-			zero++;
-			return 0;
-		} else if(k == 1) {
-			one++;
-			return 1;
-		} else {
-			return fibo(k-1) + fibo(k-2);
+		
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(br.readLine());
+			System.out.println(dp[n][0]+ " " + dp[n][1]);
 		}
 	}
 }
