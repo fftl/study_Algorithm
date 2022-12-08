dd769d0faf5951e8e39a3c65493935eee69b5b4a
이동민 <fftl0785@gmail.com>
2022-12-08 22:38:57 +0900

    update: 2420_사파리월드

---
+++ src/com/baekjoon/bronze/Main_BJ_02420_B5_사파리월드.java
@@ -0,0 +1,16 @@
+package com.baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Main_BJ_02420_B5_사파리월드 {
+
+	public static void main(String[] args) throws Exception{
+
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		
+		System.out.println(Math.abs(a-b));
+		sc.close();
+	}
+}
