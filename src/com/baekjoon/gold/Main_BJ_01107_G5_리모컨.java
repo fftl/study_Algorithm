bbff0c4fe5559a91f0fd257ea2dc5821011f066c
이동민 <fftl0785@gmail.com>
2022-12-10 21:19:33 +0900

    update: 1107_리모컨

--- src/com/baekjoon/gold/Main_BJ_01107_G5_리모컨.java
+++ src/com/baekjoon/gold/Main_BJ_01107_G5_리모컨.java
@@ -1,20 +1,72 @@
 package com.baekjoon.gold;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main_BJ_01107_G5_리모컨 {
+	static int len, t, cha, res;
+	static boolean[] check;
+	
+	static void dfs(String num) {
+		int nowNum = Integer.parseInt(num);
+		if(nowNum>1000000) return;
+		
+		if(Math.abs(t-nowNum) < res) {
+			cha = nowNum;
+			res = Math.abs(t-nowNum);
+		} else if (Math.abs(t-nowNum) == res){
+			if(nowNum < cha) {
+				cha = nowNum;
+			}
+		}
+		
+		for (int i = 0; i < check.length; i++) {
+			if(!check[i]) {
+				dfs(num+i);
+			}
+		}
+	}
 
 	public static void main(String[] args) throws Exception{
 
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.close();
+		t = sc.nextInt();
+		len = Integer.toString(t).length();
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int t = Integer.parseInt(br.readLine());
+		if(t == 100) {
+			System.out.println(0);
+			sc.close();
+			return;
+		}
+		
+		int n = sc.nextInt();
+		check = new boolean[10];
+		
+		for (int i = 0; i < n; i++) {
+			int now = sc.nextInt();
+			check[now] = true;
+		}
+//		System.out.println(Arrays.toString(check));
+		res = Integer.MAX_VALUE;
+		
+		if(!check[0]) {
+			if(Math.abs(t-0) < res) {
+				cha = 0;
+				res = Math.abs(t-0);
+			}
+		}
+		
+		for (int i = 1; i < check.length; i++) {
+			if(!check[i]) {
+				dfs(Integer.toString(i));
+			}
+		}
+		
+//		System.out.println(res);
+//		System.out.println(cha);
+		int aRes = Integer.toString(cha).length()+res;
+		int bRes = Math.abs(t-100);
+		if(aRes>0) System.out.println(Math.min(aRes, bRes));
+		else System.out.println(bRes);
+		sc.close();
 	}
 }
