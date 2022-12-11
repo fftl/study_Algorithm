4f970c0c6927bcfc29167d30f55260fce34546dd
이동민 <fftl0785@gmail.com>
2022-12-11 15:09:04 +0900

    update: 1927_최소 힙

--- src/com/baekjoon/silver/Main_BJ_01927_S2_최소힙.java
+++ src/com/baekjoon/silver/Main_BJ_01927_S2_최소힙.java
@@ -11,9 +11,13 @@ public class Main_BJ_01927_S2_최소힙 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
+		//최소 힙 역할을 해줄 PriorityQueue를 만들어 주었습니다.
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		
 		StringBuilder sb = new StringBuilder();
+		
+		//요구사항에 맞도록 입력 값이 자연수일경우 pq에 입력해주고
+		//0일 경우에는 최소값을 출력하는 동작을 반복합니다.
 		for (int i = 0; i < n; i++) {
 			int now = Integer.parseInt(br.readLine());
 			if(now == 0) {
@@ -24,6 +28,7 @@ public class Main_BJ_01927_S2_최소힙 {
 			}
 		}
 		
+		//결과물 출력!
 		System.out.println(sb.toString().trim());
 	}
 }
