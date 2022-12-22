4d25e8a74839269e2b040de75c2d37c7652d7577
이동민 <fftl0785@gmail.com>
2022-12-22 23:34:14 +0900

    update: 21758_꿀 따기

---
+++ src/com/baekjoon/gold/Main_BJ_21758_꿀따기.java
@@ -0,0 +1,72 @@
+package com.baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_21758_꿀따기 {
+	static int n;
+	static int[] honeys;
+	static long maxCount, total;
+	static long[] right, left;
+
+	static void case1() {
+		long bee1, bee2;
+
+		for (int i = 1; i <= n - 2; i++) {
+			bee1 = total - honeys[0] - honeys[i];
+			bee2 = total - right[i];
+			maxCount = Math.max(maxCount, bee1 + bee2);
+		}
+	}
+
+	static void case2() {
+		long bee1, bee2;
+
+		for (int i = n-2; i >= 1; i--) {
+			bee1 = total - honeys[n-1] - honeys[i];
+			bee2 = total - left[i];
+			maxCount = Math.max(maxCount, bee1 + bee2);
+		}
+	}
+
+	static void case3() {
+		long bee1, bee2;
+
+		for (int i = 1; i <= n-2; i++) {
+			bee1 = right[i] - honeys[0];
+			bee2 = left[i] -  honeys[n-1];
+			maxCount = Math.max(maxCount, bee1 + bee2);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+
+		honeys = new int[n];
+		right = new long[n];
+		left = new long[n];
+		long temp = 0;
+		for (int i = 0; i < n; i++) {
+			honeys[i] = Integer.parseInt(st.nextToken());
+			temp += honeys[i];
+			right[i] = temp;
+		}
+
+		temp = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			temp += honeys[i];
+			left[i] = temp;
+		}
+
+		total = right[n - 1];
+		case1();
+		case2();
+		case3();
+		System.out.println(maxCount);
+	}
+}
--- src/com/baekjoon/silver/Main_BJ_01614_S3_영식이의손가락.java
+++ src/com/baekjoon/silver/Main_BJ_01614_S3_영식이의손가락.java
@@ -9,7 +9,7 @@ public class Main_BJ_01614_S3_영식이의손가락 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a = Integer.parseInt(br.readLine());
-		int n = Integer.parseInt(br.readLine());
+		long n = Long.parseLong(br.readLine());
 		
 		if(n==0) {
 			System.out.println(a-1);
@@ -17,19 +17,30 @@ public class Main_BJ_01614_S3_영식이의손가락 {
 		}
 		
 		long result = 0;
-		if(n%2!=0) {
-			if(a==5) {
-				result = 12+(8*(n-1));
-			} else {
-				result = 5+(4*(n-1))+(4-a)%4;
-			}
-		} else {
-			if(n==1) {
-				result = 8*(n-1);
-			} else {
-				result = 5+(4*(n-1))+a-2;
-			}
-		}
+//		if(n%2!=0) {
+//			if(a==5) {
+//				result = 12+(8*(n-1));
+//			} else {
+//				result = 5+(4*(n-1))+(4-a)%4;
+//			}
+//		} else {
+//			if(n==1) {
+//				result = 8*(n-1);
+//			} else {
+//				result = 5+(4*(n-1))+a-2;
+//			}
+//		}
+		if(a == 1) {
+            result = (long)(n*8);
+        }else if(a == 2) {
+            result = (long)(1+(n/2)*8+(n%2)*6);
+        }else if(a == 3) {
+            result = (long)(2+(n/2)*8+(n%2)*4);
+        }else if(a == 4) {
+            result = (long)(3+(n/2)*8+(n%2)*2);
+        }else {
+            result = (long)(4 + n*8);
+        }
 		
 		System.out.println(result);
 	}
