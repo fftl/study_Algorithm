4104d288328b69b3ebb3a0724ab3bb91ae36d037
fftl <fftl0785@gmail.com>
2023-01-05 15:39:01 +0900

    update: 11403_경로 찾기

--- src/com/baekjoon/silver/Main_BJ_11403_S1_경로찾기.java
+++ src/com/baekjoon/silver/Main_BJ_11403_S1_경로찾기.java
@@ -2,11 +2,7 @@ package com.baekjoon.silver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main_BJ_11403_S1_경로찾기 {
 	static int N;
@@ -17,12 +13,12 @@ public class Main_BJ_11403_S1_경로찾기 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int t = Integer.parseInt(br.readLine());
-		
+		N = Integer.parseInt(br.readLine());
 		
 		arr = new ArrayList<>();
 		for (int i = 0; i < N; i++) arr.add(new ArrayList<>()); 
-		
+
+		//양방향의 경로를 ArrayList에 담아줍니다.
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
@@ -33,13 +29,35 @@ public class Main_BJ_11403_S1_경로찾기 {
 				}
 			}
 		}
-		
-		int[][] board = new int[t][t];
-		
+
+		System.out.println(arr.toString());
+		board = new int[N][N];
+
+		//i에서 갈 수 있는 now들을 담아줍니다.
 		Queue<Integer> q = new LinkedList<>();
-		for (int i = 0; i < board.length; i++) {
-			
+		for (int i = 0; i < N; i++) {
+			for (int j=0; j<arr.get(i).size(); j++){
+				int now = arr.get(i).get(j);
+
+				//만약에 i에서 j를 가본적이 없다면
+				if(board[i][now] == 0){
+					board[i][now] = 1;
+					q.add(now);
+				}
+
+				//처음 가보는 곳이 존재한다면,
+				while(!q.isEmpty()){
+					int num = q.poll();
+					for(int k=0; k<arr.get(num).size(); k++){
+						int now2 = arr.get(num).get(k);
+						if(board[i][now2] == 0) board[i][now2] = 1;
+						if(board[num][now2] == 0){
+							q.add(now2);
+						}
+					}
+				}
+			}
 		}
-		
+		System.out.println(Arrays.deepToString(board));
 	}
 }
