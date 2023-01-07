291922b130706895c3ea07fe10c236a967b9c559
이동민 <fftl0785@gmail.com>
2023-01-07 23:26:05 +0900

    update: 5337_웰컴

---
+++ src/com/baekjoon/bronze/Main_BJ_05337_B5_웰컴.java
@@ -0,0 +1,11 @@
+package com.baekjoon.bronze;
+
+public class Main_BJ_05337_B5_웰컴 {
+
+	public static void main(String[] args) throws Exception{
+		String str = ".  .   .\n" + 
+				"|  | _ | _. _ ._ _  _\n" + 
+				"|/\\|(/.|(_.(_)[ | )(/.";
+		System.out.println(str);
+	}
+}
