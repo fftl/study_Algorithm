cf06cc76e9158c372842800e20e7a956d980cd3b
이동민 <fftl0785@gmail.com>
2023-01-08 16:04:46 +0900

    update: 6064_카잉 달력

--- src/com/baekjoon/silver/Main_BJ_06064_S1_카잉달력.java
+++ src/com/baekjoon/silver/Main_BJ_06064_S1_카잉달력.java
@@ -2,7 +2,6 @@ package com.baekjoon.silver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main_BJ_06064_S1_카잉달력 {
@@ -15,8 +14,29 @@ public class Main_BJ_06064_S1_카잉달력 {
 		
 		for (int tc = 0; tc < t; tc++) {
 			st = new StringTokenizer(br.readLine());
-			int M = Integer.parseInt(br.readLine());
-			int N = Integer.parseInt(br.readLine());
+			int M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			int lcm = M*N / gcd(M, N);
+			int n = 0;
+			int ans = -1;
+			while(n*M< lcm) {
+				if((n*M + x - y) % N == 0){
+					ans = n * M + x;
+					break;
+				}
+				n++;
+			}
+			
+			System.out.println(ans);
 		}
+	}
+	
+	static int gcd(int n1, int n2) {
+		if(n2 == 0) return n1;
+		return gcd(n2, n1%n2);
 	}
 }
