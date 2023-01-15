23ee26b24b11cf78fa22052e7d8f2b62526944f1
이동민 <fftl0785@gmail.com>
2023-01-15 17:05:42 +0900

    update: 2638_치즈

--- src/com/baekjoon/Main_BJ_00.java
+++ src/com/baekjoon/Main_BJ_00.java
@@ -2,16 +2,15 @@ package com.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main_BJ_00 {
-	static int N, K;
-
 	public static void main(String[] args) throws Exception{
-		
+		Scanner sc = new Scanner(System.in);
+		sc.close();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(br.readLine());
 	}
 }
