155d899bc545f44547d75b2c7dbd32f693d5d029
이동민 <fftl0785@gmail.com>
2023-01-23 09:37:03 +0900

    update: pg_L2_튜플

--- src/com/baekjoon/silver/Main_BJ_01629_S1_곱셈.java
+++ src/com/baekjoon/silver/Main_BJ_01629_S1_곱셈.java
@@ -3,15 +3,28 @@ package com.baekjoon.silver;
 import java.util.Scanner;
 
 public class Main_BJ_01629_S1_곱셈 {
+	static long c;
+	
+	static long div(long a, long zisu) {
+		if(zisu == 1) return a%c;
+		
+		long temp = div(a, zisu/2);
+		
+		if(zisu % 2 == 1) {
+			return (temp * temp % c) * a % c;
+		}
+		
+		return temp * temp % c;
+	}
 
 	public static void main(String[] args) throws Exception{
 
 		Scanner sc = new Scanner(System.in);
 		long a = sc.nextLong();
 		long b = sc.nextLong();
-		long c = sc.nextLong();
+		c = sc.nextLong();
 		
-		long result = (long) Math.pow(a, b) %c;
+		long result = div(a, b);
 		System.out.println(result);
 		sc.close();
 	}
