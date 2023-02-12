4b3dde4d4c6a88323b633041a8b604ac1a365cab
이동민 <fftl0785@gmail.com>
2023-02-13 00:10:06 +0900

    update: 1932_정수 삼각형

---
+++ src/com/baekjoon/bronze/Main_BJ_11718_B5_그대로출력하기.java
@@ -0,0 +1,15 @@
+package com.baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Main_BJ_11718_B5_그대로출력하기 {
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			System.out.println(sc.nextLine());
+		}
+		
+		sc.close();
+	}
+}
