fc909655553b1c64ed7b054add90ff948c0f30b2
이동민 <fftl0785@gmail.com>
2023-03-06 06:48:29 +0900

    update 11660 주석 추가

--- src/com/baekjoon/silver/Main_BJ_11660_S1_구간합구하기5.java
+++ src/com/baekjoon/silver/Main_BJ_11660_S1_구간합구하기5.java
@@ -8,15 +8,19 @@ import java.util.StringTokenizer;
 public class Main_BJ_11660_S1_구간합구하기5 {
 
 	public static void main(String[] args) throws Exception {
+		
+		//입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
 		int cnt = Integer.parseInt(st.nextToken());
 		
+		//n의 크기의 board와 dp를 선언 및 초기화 시켜줍니다.
 		int[][] board = new int[n][n];
 		int[][] dp = new int[n][n];
 		
+		//board를 채워줍니다.
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < board.length; j++) {
@@ -24,6 +28,7 @@ public class Main_BJ_11660_S1_구간합구하기5 {
 			}
 		}
 		
+		//dp의 0, 0을 채워주고 크기에 맞는 누적합을 구해줍니다.
 		dp[0][0] = board[0][0];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
@@ -39,6 +44,7 @@ public class Main_BJ_11660_S1_구간합구하기5 {
 			}
 		}
 		
+		//요구사항에 맞는 sx, sy, ex, ey를 에 맞는 범위의 누적합을 구해줍니다.
 		for (int i = 0; i < cnt; i++) {
 			st = new StringTokenizer(br.readLine());
 			int sx = Integer.parseInt(st.nextToken())-1;
@@ -46,18 +52,28 @@ public class Main_BJ_11660_S1_구간합구하기5 {
 			int ex = Integer.parseInt(st.nextToken())-1;
 			int ey = Integer.parseInt(st.nextToken())-1;
 			
+			//dp[ex][ey]를 기준으로 누적합을 구하게 되기에 먼저 구해놓습니다.
 			int end = dp[ex][ey];
+			
+			//시작점, 종료점이 같다면 그냥 해당 좌표 수를 반환합니다.
 			if(sx==ex && sy==ey) {
 				System.out.println(board[sx][sy]);
 				continue;
 			}
 			
+			//시작점이 0,0 이라면 따로 계산할 필요 없이 누적합을 반환합니다.
 			if(sx == 0 && sy == 0) {
 				System.out.println(end);
+				
+			//시작점이 최상단에 붙어있다면, 왼쪽부분만 제거해주면 됩니다.
 			} else if(sx == 0){
 				System.out.println(end-dp[ex][sy-1]);
+				
+			//시작점이 가장 왼쪽에 붙어있다면 위쪽 부분만 제거해주면 됩니다.
 			} else if(sy == 0) {
 				System.out.println(end-dp[sx-1][ey]);
+			
+			//위의 모든 경우에 걸리지 않았다면 최상단, 가장 좌측을 빼주고 두가지에 중복하여 들어간 sx-1, sy-1 좌표를 더해주어 누적합을 구해줍니다.
 			} else {
 				System.out.println(end-dp[sx-1][ey]-dp[ex][sy-1]+dp[sx-1][sy-1]);
 			}
