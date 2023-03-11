a7e4dd2db63153cd9fc69c6ce13275977bbb85f6
이동민 <fftl0785@gmail.com>
2023-03-12 00:52:54 +0900

    update: 10768_특별한 날

---
+++ src/com/baekjoon/bronze/Main_BJ_10768_B4_특별한날.java
@@ -0,0 +1,20 @@
+package com.baekjoon.bronze;
+
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int month = Integer.parseInt(br.readLine());
+        int day = Integer.parseInt(br.readLine());
+        
+        if(month==1 || (month==2 && day<18)){
+            System.out.println("Before");
+        } else if(month==2 && day==18){
+            System.out.println("Special");
+        } else {
+            System.out.println("After");
+        }
+    }
+}
