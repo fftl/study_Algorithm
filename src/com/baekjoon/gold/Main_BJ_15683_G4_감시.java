9079f5ea4c84c9360b329875983d64209afbbf25
fftl <fftl0785@gmail.com>
2023-04-11 17:47:38 +0900

    update : 사다리 타기? ssafy 아직 푸는 중

--- src/com/baekjoon/gold/Main_BJ_15683_G4_감시.java
+++ src/com/baekjoon/gold/Main_BJ_15683_G4_감시.java
@@ -5,15 +5,13 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main_BJ_15683_G4_감시 {
-
-	static int Y, X, minRes, E;	//격자 크기 Y,X 결과값 minRes, 카메라 개수 E
-	static int[][] board, copyBoard;	//입력으로 주어지는 board와 각 카메라의 회전마다 생겨질 copyBoard 입니다.
-	static int[] dy = { -1, 0, 1, 0 };	//회전을 표현할 dy, dx
+public class Main_BJ_15683_G4_감시{
+	static int Y, X, minRes, E;
+	static int[][] board, copyBoard;
+	static int[] dy = { -1, 0, 1, 0 };
 	static int[] dx = { 0, 1, 0, -1 };
-	static ArrayList<Pair> camera;	//카메라들을 담아줄 ArrayList
+	static ArrayList<Pair> camera;
 
-	//카메라의 좌표 y, x와 카메라의 종류 k 그리고 카메라가 보고있는 뱡항 d를 표시해줍니다.
 	public static class Pair {
 		int y, x, k, d;
 
@@ -32,50 +30,137 @@ public class Main_BJ_15683_G4_감시 {
 	}
 
 	public static void main(String[] args) throws Exception {
-
-		//데이터 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		Y = Integer.parseInt(st.nextToken());
 		X = Integer.parseInt(st.nextToken());
 
 		board = new int[Y][X];
-		minRes = Y*X; //최소값을 구해야 하기 때문에 초기에는 최대 값으로 줍니다.
+		minRes = Y * X; // 최소값을 구해야 하기 때문에 초기에는 최대 값으로 줍니다.
 		camera = new ArrayList<>();
 
 		for (int i = 0; i < Y; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < X; j++) {
 				int num = Integer.parseInt(st.nextToken());
-				if(num>0 && num<6) {
-					camera.add(new Pair(i, j, num));
+				if (num > 0 && num < 5) {
+					camera.add(new Pair(i, j, num, 0));
 				}
 				board[i][j] = num;
 			}
 		}
 
-		//카메라 개수 확인
+		// 5번은 모든 방향이므로 dfs에 포함시키기에는 비효율적입니다.
+		// 미리 5번은 board에 표시해준 뒤 해당 board를 이용해 dfs를 시작하도록 합니다.
+		copyBoard = copyBoard();
+		for (int i = 0; i < Y; i++) {
+			for (int j = 0; j < X; j++) {
+				if (board[i][j] == 5) {
+					look(new Pair(i, j, 5, 0));
+				}
+			}
+		}
+
+		for (int i = 0; i < Y; i++) {
+			for (int j = 0; j < X; j++) {
+				board[i][j] = copyBoard[i][j];
+			}
+		}
+
 		E = camera.size();
 		dfs(0);
+
+		System.out.println(minRes);
+	}
+
+	static boolean isRange(int y, int x) {
+		return (0 <= y && y < Y && 0 <= x && x < X);
+	}
+
+	static void lookRun(int y, int x, int d) {
+		int ny = y + dy[d];
+		int nx = x + dx[d];
+
+		while (isRange(ny, nx)) {
+			if (copyBoard[ny][nx] == 6)
+				break; // 벽을 만나면 바로 종료합니다.
+			else { // 벽이 아니고
+				if (copyBoard[ny][nx] == 0) { // 아직 감시되고 있지 않다면
+					copyBoard[ny][nx] = -1;
+				}
+			}
+			ny = ny + dy[d];
+			nx = nx + dx[d];
+		}
+	}
+
+	// y,x 좌표에서 k를 바라보고 있다는 표시를 해줍니다.
+	static void look(Pair pair) {
+		int y = pair.y;
+		int x = pair.x;
+		int k = pair.k;
+		int d = pair.d;
+
+		switch (k) {
+			case 1:
+				lookRun(y, x, (d + 1) % 4);
+				break;
+			case 2:
+				lookRun(y, x, (d + 1) % 4);
+				lookRun(y, x, (d + 3) % 4);
+				break;
+			case 3:
+				lookRun(y, x, d);
+				lookRun(y, x, (d + 1) % 4);
+				break;
+			case 4:
+				lookRun(y, x, d);
+				lookRun(y, x, (d + 1) % 4);
+				lookRun(y, x, (d + 3) % 4);
+				break;
+
+			case 5: // 모든방향! 회전해도 변화가 없기에 최초에 그냥 일괄처리를 합니다.
+				lookRun(y, x, d);
+				lookRun(y, x, (d + 1) % 4);
+				lookRun(y, x, (d + 2) % 4);
+				lookRun(y, x, (d + 3) % 4);
+				break;
+		}
+	}
+
+	// 각 카메라 배치마다 새로운 맵을 그려줘야 하기 때문에 board를 복사해줍니다.
+	static int[][] copyBoard() {
+		int[][] copy = new int[Y][X];
+		for (int i = 0; i < Y; i++) {
+			for (int j = 0; j < X; j++) {
+				copy[i][j] = board[i][j];
+			}
+		}
+		return copy;
 	}
 
 	static void dfs(int cnt) {
-		if(cnt==E) {
+		// 카메라 세개를 다 뽑았다면 연산을 시작합니다.
+		if (cnt == E) {
+			copyBoard = copyBoard();
+			for (int i = 0; i < camera.size(); i++) {
+				look(camera.get(i));
+			}
 			minCheck();
 			return;
 		}
 
-		for (int i = cnt; i < E; i++) {
-			for (int j = 1; j <= 5; j++) {
-				System.out.println(i+","+j);
-				camera.get(i).k=j;
-				dfs(cnt+1);
-			}
+		/**
+		 * 틀린 코드 for (int i = cnt; i < E; i++) { for(int j=1; j<=5; j++) {
+		 * camera.get(cnt).k = j; dfs(cnt+1); } }
+		 */
+
+		for (int j = 0; j <= 3; j++) {
+			camera.get(cnt).d = j;
+			dfs(cnt + 1);
 		}
 	}
 
-	//카메라를 배치한뒤 볼수 있는 방향을 표시하고, 사각지대의 개수를 세어줍니다.
-	//만약 최소값이 나왔다면 갱신해줍니다.
 	static void minCheck() {
 		int cnt = 0;
 		for (int i = 0; i < Y; i++) {
---
+++ src/com/baekjoon/gold/Main_BJ_15684_G3_사다리조작.java
@@ -0,0 +1,159 @@
+package com.baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main_BJ_15684_G3_사다리조작 {
+	static int X, M, Y;
+	static HashMap<int[], int[]> def, ref;
+	static boolean[][] board, copy;
+	static boolean end;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		X = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		Y = Integer.parseInt(st.nextToken());
+		def = new HashMap<>();
+		
+		board = new boolean[Y][X];
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken())-1;
+			board[r][c] = true;
+			board[r][c+1] = true;
+			def.put(new int[]{r, c}, new int[]{r, c+1});
+			def.put(new int[]{r, c+1}, new int[]{r, c});
+		}
+
+
+		copy = copyBoard();
+		print();
+		if(!sadariCheck()){
+			return;
+		}
+
+		end = false;
+		boolean[][] visited = new boolean[Y][X];
+
+		if(runCheck()) {
+			System.out.println(0);
+			return;
+		}
+		dfs(0, 1, visited);
+		if(end) {
+			System.out.println(1);
+			return;
+		}
+		dfs(0, 2, visited);
+		if(end) {
+			System.out.println(2);
+			return;
+		}
+		dfs(0, 3, visited);
+		if(end) {
+			System.out.println(3);
+		}else{
+			System.out.println(-1);
+		}
+	}
+
+	//처음에 놓여있는 사다리를 포함한 맵을 복사합니다.
+	static boolean[][] copyBoard(){
+
+		boolean[][] copy = new boolean[Y][X];
+		for (int i = 0; i < Y; i++) {
+			for (int j = 0; j < X; j++) {
+				copy[i][j] = board[i][j];
+			}
+		}
+		return copy;
+	}
+	
+	//사다리의 모든 경우를 확인합니다.
+	static void dfs(int cnt, int maxCnt, boolean[][] visited){
+		if(end) return;
+
+		if(cnt == maxCnt){
+			for (int i = 0; i < Y; i++) {
+				for (int j = 0; j < X; j++) {
+					if (visited[i][j]) copy[i][j] = true;
+				}
+			}
+			if(runCheck()){
+				end = true;
+				return;}
+
+			copy = copyBoard();
+			return;
+		}
+
+		for (int i = 0; i < Y; i++) {
+			for (int j = 0; j < X-1; j++) {
+				if(copy[i][j] || visited[i][j]) continue;
+
+				visited[i][j] = true;
+				visited[i][j+1] = true;
+				dfs(cnt+1, maxCnt, visited);
+				visited[i][j] = false;
+				visited[i][j+1] = false;
+			}
+		}
+	}
+
+	static boolean sadariCheck(){
+		for (int i = 0; i < Y; i++) {
+			int max = copy[i][0] ? 1 : 0;
+			for (int j = 1; j < X; j++) {
+				if(copy[i][j-1] && copy[i][j]) max++;
+				else if (!copy[i][j-1] && copy[i][j]){
+					max=1;
+				}
+			}
+			if(max>=3) return false;
+		}
+		return true;
+	}
+
+	//사다리가 i->i로 이동하는지 확인
+	static boolean runCheck() {
+		for (int j = 0; j < X; j++) {
+			int s = 0;
+			int x = j;
+			while(true){
+				if(s==Y){
+					if(x != j) return false; //다른곳으로 도착했다면 false
+					else break; //같은 곳으로 도착했다면 다음 줄을 확인합니다.
+				}
+
+				if(copy[s][j]){
+					if(0<=x-1 && copy[s][x-1]) {
+						x--;
+					} else if(x+1<X&& copy[s][x+1]) {
+						x++;
+					}
+					s++;
+				} else {
+					s++;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static void print(){
+		for (int i = 0; i < Y; i++) {
+			String str = "";
+			for (int j = 0; j < X; j++) {
+				if (copy[i][j]) str += "1 ";
+				else str += "0 ";
+			}
+			System.out.println(str);
+		}
+	}
+}
