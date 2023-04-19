b510b2d914cdeea405a1f82d31b982912f1190ac
이동민 <fftl0785@gmail.com>
2023-04-20 00:29:25 +0900

    update : 230420_home

---
+++ src/com/swexpertacademy_pro/Solution_14618_끝말잇기2_user.java
@@ -0,0 +1,13 @@
+package com.swexpertacademy_pro;
+
+public class Solution_14618_끝말잇기2_user {
+    public void init(int N, int M, char[][] mWords)
+    {
+
+    }
+
+    public int playRound(int mID, char mCh)
+    {
+        return 0;
+    }
+}
