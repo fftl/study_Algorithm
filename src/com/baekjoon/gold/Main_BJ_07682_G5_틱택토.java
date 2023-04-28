651d1179dc60e8ecc4dfe84e919f7b62f2dafb5c
fftl <fftl0785@gmail.com>
2023-04-28 09:24:43 +0900

    update : 틱택토

--- src/com/baekjoon/gold/Main_BJ_07682_G5_틱택토.java
+++ src/com/baekjoon/gold/Main_BJ_07682_G5_틱택토.java
@@ -59,6 +59,7 @@ public class Main_BJ_07682_G5_틱택토 {
 			for (int i = 0; i < 3; i++) {
 				if(str.charAt(i) == str.charAt(i+3) && str.charAt(i+3) == str.charAt(i+6) && str.charAt(i) == 'O') oYes = true;
 				if(str.charAt(i) == str.charAt(i+3) && str.charAt(i+3) == str.charAt(i+6) && str.charAt(i) == 'X') xYes = true;
+				if(xCnt+oCnt==9 && oYes) return false;
 			}
 			if(!oYes && xYes) return true;
 			
@@ -67,12 +68,15 @@ public class Main_BJ_07682_G5_틱택토 {
 			for (int i = 0; i < 3; i++) {
 				if(str.charAt(i*3) == str.charAt(i*3+1) && str.charAt(i*3+1) == str.charAt(i*3+2) && str.charAt(i*3) == 'O') oYes = true;
 				if(str.charAt(i*3) == str.charAt(i*3+1) && str.charAt(i*3+1) == str.charAt(i*3+2) && str.charAt(i*3) == 'X') xYes = true;
+				if(xCnt+oCnt==9 && oYes) return false;
 			}
 			if(!oYes && xYes) return true;
 			
 			if(str.charAt(0) == str.charAt(4) && str.charAt(4) == str.charAt(8) && str.charAt(4) == 'X') return true;
 			if(str.charAt(2) == str.charAt(4) && str.charAt(4) == str.charAt(6) && str.charAt(4) == 'X') return true;
-			
+
+			if(xCnt+oCnt==9 && str.charAt(0) == str.charAt(4) && str.charAt(4) == str.charAt(8) && str.charAt(4) == 'O') return false;
+			if(xCnt+oCnt==9 && str.charAt(2) == str.charAt(4) && str.charAt(4) == str.charAt(6) && str.charAt(4) == 'O') return false;
 			if(xCnt+oCnt==9) return true;
 		}
 		
