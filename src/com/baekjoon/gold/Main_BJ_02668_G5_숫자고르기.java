5db546a6220270e04d6c35df1d6c1a0f0258cc67
fftl <fftl0785@gmail.com>
2023-05-22 15:16:21 +0900

    update

--- src/com/baekjoon/gold/Main_BJ_02668_G5_숫자고르기.java
+++ src/com/baekjoon/gold/Main_BJ_02668_G5_숫자고르기.java
@@ -6,16 +6,18 @@ import java.util.Scanner;
 public class Main_BJ_02668_G5_숫자고르기 {
 
 	public static void main(String[] args) throws Exception{
-
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] base = new int[n+1];
 		int[] arr = new int[n+1];
+		boolean[] visited = new boolean[2];
 		
 		for (int i = 1; i <= n; i++) {
 			base[i] = i;
-			arr[i] = sc.nextInt();
+			int num = sc.nextInt();
 		}
+
+		int result = 0;
 		
 		System.out.println(Arrays.toString(base));
 		System.out.println(Arrays.toString(arr));
---
+++ src/com/baekjoon/gold/Main_BJ_test.java
@@ -0,0 +1,14 @@
+package com.baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main_BJ_test {
+	public static void main(String[] args) throws Exception{
+		ArrayList<Integer> arr = new ArrayList<>();
+		System.out.println(arr.size());
+	}
+}
