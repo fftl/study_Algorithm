5cc0da78b238665e259a9b7974778f80e481e448
fftl <fftl0785@gmail.com>
2023-05-24 17:52:56 +0900

    update

--- src/com/baekjoon/silver/Main_BJ_04673_S5_셀프넘버.java
+++ src/com/baekjoon/silver/Main_BJ_04673_S5_셀프넘버.java
@@ -1,10 +1,5 @@
 package com.baekjoon.silver;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
 public class Main_BJ_04673_S5_셀프넘버 {
 	public static void main(String[] args) throws Exception{
 		boolean[] check = new boolean[10001];
