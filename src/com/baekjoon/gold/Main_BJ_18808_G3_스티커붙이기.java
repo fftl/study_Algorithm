3d9086e127b2162ffd0556fcff02dc6078606344
이동민 <fftl0785@gmail.com>
2023-08-07 23:12:45 +0900

    bj 18808

--- src/com/baekjoon/gold/Main_BJ_18808_G3_스티커붙이기.java
+++ src/com/baekjoon/gold/Main_BJ_18808_G3_스티커붙이기.java
@@ -2,7 +2,6 @@ package com.baekjoon.gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main_BJ_18808_G3_스티커붙이기 {
@@ -30,30 +29,81 @@ public class Main_BJ_18808_G3_스티커붙이기 {
 			for (int j = 0; j < y; j++) {
 				st = new StringTokenizer(br.readLine());
 				for (int k = 0; k < x; k++) {
-					if(st.nextToken().equals(1)) now[j][k] = true;
+					if(st.nextToken().equals("1")) now[j][k] = true;
 					else now[j][k] = false;
 				}
 			}
 			
 			//받아낸 스티커 now를 board에 대입해봅니다.
-			
+			insert(now, y, x);
+//			System.out.println(i+"---------------------------------------------");
 		}
-	}
-	
-	public static boolean insert(boolean[][] now, int y, int x) {
-		int a = 0;
-		int b = 0;
 		
+		int res = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
+				if(board[i][j]) res++;
+			}
+		}
+		
+		System.out.println(res);
+	}
+	
+	public static void insert(boolean[][] now, int y, int x) {
+		
+		boolean end = false;
+		
+		
+		for (int t = 0; t < 4; t++) {
+			int r = now.length;
+			int c = now[0].length;
+			
+			if(end) break;
+			i : for (int i = 0; i <= N-r; i++) {
+				j : for (int j = 0; j <= M-c; j++) {
+					
+					for (int k = 0; k < r; k++) {
+						for (int w = 0; w < c; w++) {
+							//노트북과, 스티커가 겹치는 부분이 있다면 해당 위치에는 못놓습니다.
+							if(board[i+k][j+w] && now[k][w]) {
+								continue j;
+							}
+						}
+					}
+					
+					//여기까지 왔다면 스티커를 놓을 수 있는 상태입니다.
+					//board에 스티커를 채워줍니다.
+					for (int k = 0; k < r; k++) {
+						for (int w = 0; w < c; w++) {
+							//노트북과, 스티커가 겹치는 부분이 있다면 해당 위치에는 못놓습니다.
+							if(now[k][w]) board[i+k][j+w] = now[k][w];
+						}
+					}
+					
+//					System.out.println("현재상황입니다 --");
+//					System.out.println(Arrays.deepToString(board));
+					end = true;
+					break i;
+				}
+			}
+			if(!end) { 
+				now = turn(now, r, c);
 				
 			}
 		}
-		return false;
 	}
 	
 	//회전 시키는 것을 표현해줍니다.
-	public static boolean[][] turn(boolean[][] now) {
-		return null;
+	public static boolean[][] turn(boolean[][] now, int y, int x) {
+		boolean[][] newStk = new boolean[x][y];
+		
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				newStk[i][j] = now[y-1-j][i];
+			}
+		}
+		
+//		System.out.println(Arrays.deepToString(newStk));
+		return newStk;
 	}
 }
