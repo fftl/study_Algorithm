dcc4277cf3260993a1fb522139f1f7b81c27edec
이동민 <fftl0785@gmail.com>
2023-08-14 01:45:44 +0900

    update

--- src/com/swexpertacademy_pro/Solution_13072_병사관리_user.java
+++ src/com/swexpertacademy_pro/Solution_13072_병사관리_user.java
@@ -1,32 +1,25 @@
 package com.swexpertacademy_pro;
 
+//각 팀 별로 스코어 5개의 맵을 전부 만든다!?
 import java.util.HashMap;
-import java.util.List;
 
 public class Solution_13072_병사관리_user {
 
-    static HashMap<Integer, Member> members;
-    static HashMap<Integer, List<Member>> teams;
-
+    static HashMap<Integer, Team> teams = new HashMap<>();
     public void init()
     {
-        members = new HashMap<>();
-        teams = new HashMap<>();
     }
 
     public void hire(int mID, int mTeam, int mScore)
     {
-        members.put(mID, new Member(mID, mTeam, mScore));
     }
 
     public void fire(int mID)
     {
-        members.remove(mID);
     }
 
     public void updateSoldier(int mID, int mScore)
     {
-        
     }
 
     public void updateTeam(int mTeam, int mChangeScore)
@@ -38,13 +31,21 @@ public class Solution_13072_병사관리_user {
         return 0;
     }
 
+
+    static class Team{
+        int mTeam;
+
+    }
+
     static class Member{
         int mId, mTeam, mScore;
+
         public Member(int mId, int mTeam, int mScore){
             this.mId = mId;
             this.mTeam = mTeam;
             this.mScore = mScore;
         }
+
         @Override
         public String toString() {
             return "Member{" +
