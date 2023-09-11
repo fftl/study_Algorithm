028b7708fdad351a660f4497413799b6f78927e0
이동민 <fftl0785@gmail.com>
2023-09-12 00:28:37 +0900

    bj 1781

--- src/com/baekjoon/gold/Main_BJ_01781_G2_컵라면.java
+++ src/com/baekjoon/gold/Main_BJ_01781_G2_컵라면.java
@@ -2,10 +2,7 @@ package com.baekjoon.gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main_BJ_01781_G2_컵라면 {
 	public static void main(String[] args) throws Exception{
@@ -27,10 +24,6 @@ public class Main_BJ_01781_G2_컵라면 {
 			list.add(new int[] {dead, cnt, i});
 		}
 		
-//		for (int i = 1; i <= n; i++) {
-//			System.out.println(Arrays.toString(list.get(i)));
-//		}
-//		
 		Collections.sort(list, new Comparator<int[]>() {
 			
 			@Override
@@ -42,27 +35,31 @@ public class Main_BJ_01781_G2_컵라면 {
 				}
 			}
 		});
-		
-//		System.out.println("-----------------------------------------");
-//		for (int i = 1; i <= n; i++) {
-//			System.out.println(Arrays.toString(list.get(i)));
-//		}
-//		
-		int result = 0;
-//		for (int i = 1; i <= n; i++) {
-//			int[] now = list.get(i);
-//			if(deadCheck[now[0]]) { 
-//				result+=now[1];
-//				deadCheck[now[0]] = false;
-//			}
-//		}
-		
-		int idx = 1;
-		int cnt = 0;
-		while(idx<n) {
-			
+
+		long result = 0;
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+		for(int[] now : list){
+			if(now[0]==0)continue;
+
+			int size = pq.size();
+			if(size<now[0]){
+				pq.offer(now[1]);
+			}
+			else if(size == now[0]){
+				int nCount = pq.peek();
+				if(nCount < now[1]){
+					pq.poll();
+					pq.offer(now[1]);
+				}
+			}
 		}
-		
+
+		while(!pq.isEmpty()){
+			result += pq.poll();
+		}
+
+
 		System.out.println(result);
 		
 		
