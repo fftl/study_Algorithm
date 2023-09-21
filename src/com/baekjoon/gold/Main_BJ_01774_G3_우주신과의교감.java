fbd288dd46bcb42e9648e0b04cd82a272446134f
fftl <fftl0785@gmail.com>
2023-09-21 21:15:28 +0900

    bj 1774 ing

---
+++ src/com/baekjoon/gold/Main_BJ_01774_G3_우주신과의교감.java
@@ -0,0 +1,16 @@
+package com.baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main_BJ_01774_G3_우주신과의교감 {
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+	}
+}
--- src/com/baekjoon/silver/Main_BJ_05397_S2_키로거.java
+++ src/com/baekjoon/silver/Main_BJ_05397_S2_키로거.java
@@ -1,9 +1,11 @@
+package com.baekjoon.silver;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Main {
+public class Main_BJ_05397_S2_키로거 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
