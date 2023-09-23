5c579843ae1ac146b81b3954b0d4ec345aea92ab
이동민 <fftl0785@gmail.com>
2023-09-23 15:28:42 +0900

    bj 17009

--- src/com/baekjoon/Main_BJ_00.java
+++ src/com/baekjoon/Main_BJ_00.java
@@ -7,8 +7,6 @@ import java.util.StringTokenizer;
 
 public class Main_BJ_00 {
 	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		sc.close();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(br.readLine());
