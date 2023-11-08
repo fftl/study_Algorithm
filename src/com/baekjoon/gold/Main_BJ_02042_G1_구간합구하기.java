45f869625c1ff4b8a20c810098408a2ec4a8d7c6
fftl <fftl0785@gmail.com>
2023-11-08 20:50:40 +0900

    bj 1120

--- src/com/baekjoon/gold/Main_BJ_02042_G1_구간합구하기.java
+++ src/com/baekjoon/gold/Main_BJ_02042_G1_구간합구하기.java
@@ -5,53 +5,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_BJ_02042_G1_구간합구하기 {
-	static int N, M, K;
 	static long[] nums;
 	
-	static class SegmentTree{
-		long[] tree;
-		int treeSize;
+	public static class Seg{
+		long[] nodes;
 		
-		//초기화 할 때 배열의 크기를 가지고 long 배열과 트리의 크기를 구해줍니다.
-		public SegmentTree(int arrSize) {
-			int h = (int) Math.ceil(Math.log(arrSize) / Math.log(2));
-			this.treeSize = (int) Math.pow(2, h+1);
-			tree = new long[treeSize];
+		public Seg(int i) {
+			nodes = new long[i*4];
 		}
 		
-		//재귀를 이용해 각 트리의 자식들을 합하는 방식으로 모든 단계의? 합을 구합니다.
-		public long init(long[] arr, int node, int start, int end) {
-			if(start == end) {
-				return tree[node] = arr[start];
+		public long init(int node, int start, int end) {
+			if(start==end) {
+				return nodes[node] = nums[start];
 			}
-
-			//현재 노드의 왼쪽아래 + 오른쪽 아래 노드의 합을 리턴.
-			return tree[node] = init(arr, node*2, start, (start+end)/2) +
-					init(arr, node*2+1, (start+end)/2+1, end);
+			
+			return nodes[node] = init(node*2, start, (start+end)/2) + 
+					init(node*2+1, (start+end)/2+1, end);
 		}
 		
-		//중간의 값이 변경되는 것을 표현합니다.
-		//무슨 연산을 하던 node는 1부터 시작합니다.
-		public void update(int node, int start, int end, int idx, long diff) {
-			//변경될 값의 idx가 범위를 벗어날 경우 종료합니다.
+		public void	 update(int node, int start, int end, int idx, long cha) {
 			if(idx < start || end < idx) return;
+			nodes[node] += cha;
 			
-			tree[node] += diff;
-			
-			if(start != end) {
-				update(node*2, start, (start+end)/2, idx, diff);
-				update(node*2+1, (start+end)/2+1, end, idx, diff);
+			if(start != end) {				
+				update(node*2, start, (start+end)/2, idx, cha);
+				update(node*2+1, (start+end)/2+1, end, idx, cha);
 			}
 		}
 		
-		//start ~ end 의 합을 구합니다.
-		public long sum(int node, int start, int end, int left, int right) {
-			if(left > end || right < start) {
+		public long sum(int node, int start, int end, int left, int right){
+			if(end < left || right < start) {
 				return 0;
 			}
 			
-			if(left <= start && end <= right) {
-				return tree[node];
+			if(left<=start && end <=right) {
+				return nodes[node];
 			}
 			
 			return sum(node*2, start, (start+end)/2, left, right) + sum(node*2+1, (start+end)/2+1, end, left, right); 
@@ -61,10 +49,9 @@ public class Main_BJ_02042_G1_구간합구하기 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
 		nums = new long[N+1];
 		
@@ -72,22 +59,21 @@ public class Main_BJ_02042_G1_구간합구하기 {
 			nums[i] = Long.parseLong(br.readLine());
 		}
 		
-		SegmentTree tree = new SegmentTree(N);
-		tree.init(nums, 1, 1, N);
-		StringBuilder sb = new StringBuilder();
+		Seg seg = new Seg(N);
+		seg.init(1, 1, N);
 		
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < M+K; i++) {
 			st = new StringTokenizer(br.readLine());
+			int key = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			long c = Long.parseLong(st.nextToken());
+			long b = Long.parseLong(st.nextToken());
 			
-			//수 변경하기
-			if(a==1) {
-				tree.update(1, 1, N, b,  c-nums[b]);
-				nums[b] = c;
-			} else if(a==2) {
-				sb.append(tree.sum(1, 1, N, b, (int)c) + "\n");
+			if(key == 1) {
+				seg.update(1, 1, N, a, b-nums[a]);
+				nums[a] = b;
+			} else {
+				sb.append(seg.sum(1, 1, N, a, (int)b)+"\n");
 			}
 		}
 		
---
+++ src/com/baekjoon/platinum/Main_BJ_06549_P5_히스토그램에서가장큰직사각형.java
@@ -0,0 +1,22 @@
+package com.baekjoon.platinum;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main_BJ_06549_P5_히스토그램에서가장큰직사각형 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			
+			if(N==0) break;
+		}
+	}
+}
