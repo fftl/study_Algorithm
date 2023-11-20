4d4ec8435df05612f6f7475a9117403e133b7a1d
이동민 <fftl0785@gmail.com>
2023-11-21 00:06:20 +0900

    bj 10868

--- src/com/baekjoon/gold/Main_BJ_10868_G1_최솟값.java
+++ src/com/baekjoon/gold/Main_BJ_10868_G1_최솟값.java
@@ -2,52 +2,55 @@ package com.baekjoon.gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main_BJ_10868_G1_최솟값 {
-
-	static int N, M;
-	static int[] nums;
-	static TreeSet<Integer> sort;
+	static int[] arr, minTree;
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		nums = new int[N];
-		sort = new TreeSet<>();
-
-		for (int i = 0; i < N; i++) {
-			nums[i] = Integer.parseInt(br.readLine());
-			sort.add(nums[i]);
+		arr = new int[N+1];
+		for (int i = 1; i <= N ; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
+
+		minTree = new int[N*4];
+
+		minInit(1, N, 1);
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken())-1;
-			int e = Integer.parseInt(st.nextToken())-1;
 
-			Set<Integer> set = new HashSet<>();
-			for (int j = s; j <= e; j++) {
-				set.add(nums[j]);
-			}
+			int left = Integer.parseInt(st.nextToken());
+			int right = Integer.parseInt(st.nextToken());
 
-			for (int min : sort){
-				if(set.contains(min)){
-					sb.append(min+"\n");
-					break;
-				}
-			}
+			sb.append(minFind(1, N, 1, left, right) + "\n");
 		}
 
 		System.out.println(sb.toString().trim());
-
-
 	}
+
+	public static int minInit(int start, int end, int node){
+		if(start == end){
+			return minTree[node] = arr[start];
+		}
+
+		int mid = (start + end)/2;
+		return minTree[node] = Math.min(minInit(start, mid, node*2), minInit(mid+1, end, node*2+1));
+	}
+
+	public static int minFind(int start, int end, int node, int left, int right){
+		if(right<start || end <left) return Integer.MAX_VALUE;
+		if(left<=start && end<=right) return minTree[node];
+
+		int mid = (start+end)/2;
+
+		return Math.min(minFind(start, mid, node*2, left, right), minFind(mid+1, end, node*2+1, left, right));
+	}
+
 }
