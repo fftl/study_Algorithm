ae3c15ecd49364957a8cbd991654e8535f09eb6e
이동민 <fftl0785@gmail.com>
2024-01-06 04:20:27 +0900

    bj 1010

--- src/com/baekjoon/silver/Main_BJ_01010_S5_다리놓기.java
+++ src/com/baekjoon/silver/Main_BJ_01010_S5_다리놓기.java
@@ -2,40 +2,29 @@ package com.baekjoon.silver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
+//https://comain.tistory.com/182
 public class Main_BJ_01010_S5_다리놓기 {
-	static int cnt, n, m;
-	
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int tc = Integer.parseInt(br.readLine());
-		
-		for (int i = 0; i < tc; i++) {
-			st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			m = Integer.parseInt(st.nextToken());
-			
-			cnt = 0;
-			
-			dfs(0, 0, 1);
-			
-			System.out.println(cnt);
-		}
-	}
-	
-	static void dfs(int a, int b, int depth) {
-		if(depth == n) {
-			cnt++;
-		}
-		
-		for (int i = a; i < n; i++) {
-			for (int j = b; j < m; j++) {
-				dfs(i, j, depth + 1);
+		int T = Integer.parseInt(br.readLine());
+
+		for(int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			long N = Long.parseLong(st.nextToken());
+			long M = Long.parseLong(st.nextToken());
+			//result는 연산값을 저장해줄 것인데 처음에 곱하기때문에 1로 선언해준다.
+			long result = 1;
+
+			//곱하고 나누기를 반복할때마다 처리해준다.
+			//나누기 처음 값은 1이지만 반복문은 j가 0부터 시작하기 때문에 +1을 해준다.
+			for(int j = 0; j < N; j++) {
+				result *= (M - j);
+				result /= (j + 1);
 			}
+			System.out.println(result);
 		}
-		
 	}
 }
