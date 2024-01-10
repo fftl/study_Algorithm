3d90a214de761862b0169bab7482f7140c44a218
이동민 <fftl0785@gmail.com>
2024-01-11 00:03:47 +0900

    bj 17780 ing

--- src/com/baekjoon/gold/Main_BJ_17780_G2_새로운게임.java
+++ src/com/baekjoon/gold/Main_BJ_17780_G2_새로운게임.java
@@ -2,12 +2,107 @@ package com.baekjoon.gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main_BJ_17780_G2_새로운게임 {
+	static int N, K;
+	static int[][] board;
+	static int[] dy = {0, 0, -1, 1};
+	static int[] dx = {1, -1, 0, 0};
+	static Deque<Unit>[][] dq;
+	static HashMap<Integer, int[]> index;
+
+	static class Unit{
+		int id, k;
+
+		public Unit(int id, int k) {
+			this.id = id;
+			this.k = k;
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder("Unit{");
+			sb.append("id=").append(id);
+			sb.append(", k=").append(k);
+			sb.append('}');
+			return sb.toString();
+		}
+	}
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		board = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		dq = new Deque[N][N];
+		index = new HashMap<>();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				dq[i][j] = new ArrayDeque();
+			}
+		}
+
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int y = Integer.parseInt(st.nextToken())-1;
+			int x = Integer.parseInt(st.nextToken())-1;
+			int k = Integer.parseInt(st.nextToken())-1;
+
+			dq[y][x].add(new Unit(i, k));
+
+			index.put(i, new int[]{y, x});
+		}
+		
+		int time = 0;
+		while(end(time)){
+			
+		}
+	}
+
+	static boolean end(int time){
+		if(time>1000) return false;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(dq[i][j].size()==4) return false;
+			}
+		}
+		
+		return true;
+	}
+
+	//현재 dq 두개를 이용해서 해당 id 체스말의 위에 있는 애들만 옮겨주고 아닌 애들은
+	//다시 남기는 부분을 진행하고 있음
+	static void move(){
+		for (int i = 0; i < K; i++) {
+			int[] now = index.get(i);
+			Deque<Unit> temp = new ArrayDeque<>();
+			Unit nu = dq[now[0]][now[1]].peekFirst();
+			if(nu.id == i){
+				int ny = now[0] + dy[nu.k];
+				int nx = now[1] + dx[nu.k];
+				if(0<=ny && ny<N && 0<=nx && nx<N && board[ny][nx] != 2){
+					if(board[ny][nx] == 0){
+
+					}
+				} else {
+
+				}
+			} else {
+			}
+
+		}
 	}
 }
--- src/com/baekjoon/silver/Main_BJ_14501_S3_퇴사.java
+++ src/com/baekjoon/silver/Main_BJ_14501_S3_퇴사.java
@@ -2,17 +2,35 @@ package com.baekjoon.silver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main_BJ_14501_S3_퇴사 {
 	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		sc.close();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		int n = Integer.parseInt(br.readLine());
-		
-		
+
+		ArrayList<int[]> arr = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int d = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+
+			arr.add(new int[]{d, p});
+		}
+
+		int[] dp = new int[n+1];
+
+		for (int i = 0; i < n; i++) {
+			if(i+arr.get(i)[0] <= n){
+				dp[i+arr.get(i)[0]] = Math.max(dp[i+arr.get(i)[0]], dp[i]+arr.get(i)[1]);
+			}
+
+			dp[i+1] = Math.max(dp[i+1], dp[i]);
+		}
+
+		System.out.println(dp[n]);
 	}
 }
