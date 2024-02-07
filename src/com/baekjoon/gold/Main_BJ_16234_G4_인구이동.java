ebe69acdd00163170f772598ae951dfa52cb8ae5
unknown <fftl0785>
2024-02-07 21:04:04 +0900

    bj 16234

---
+++ src/com/baekjoon/gold/Main_BJ_16234_G4_인구이동.java
@@ -0,0 +1,19 @@
+package com.baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_16234_G4_인구이동 {
+	static int N,L,R;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+
+
+	}
+}
---
+++ src/com/baekjoon/silver/Main_BJ_01904_S3_01타일.java
@@ -0,0 +1,17 @@
+package com.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_01904_S3_01타일 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] dp = new int[n+1];
+
+		dp[0] = 0;
+		dp[1] = 1;
+	}
+}
