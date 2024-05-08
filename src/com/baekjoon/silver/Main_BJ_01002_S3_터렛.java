4d3821a37f83436b9018eb078e8ef4de2c7c0cdf
unknown <fftl0785>
2024-05-08 19:49:12 +0900

    bj 1002

--- src/com/baekjoon/silver/Main_BJ_01002_S3_터렛.java
+++ src/com/baekjoon/silver/Main_BJ_01002_S3_터렛.java
@@ -2,6 +2,9 @@ package com.baekjoon.silver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main_BJ_01002_S3_터렛 {
@@ -13,43 +16,35 @@ public class Main_BJ_01002_S3_터렛 {
 		int T = Integer.parseInt(br.readLine());
 		
 		for (int tc = 0; tc < T; tc++) {
-			System.out.println("TC-----------------------------");
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int r1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-			int r2 = Integer.parseInt(st.nextToken()); 
-			
-			int cnt = 0;
-			for(int i=0; i<r1; i++) {
-				int ny = y1+r1-i;
-				int nx = x1+i;
-				System.out.println(ny+","+nx);
+			int r2 = Integer.parseInt(st.nextToken());
+
+			if(x1==x2 && y1==y2 && r1==r2){
+				System.out.println(-1);
+				continue;
 			}
-			
-			System.out.println("-----------------------");
-			
-			for(int i=0; i<r1; i++) {
-				int ny = y1-r1+i;
-				int nx = x1+i;
-				System.out.println(ny+","+nx);
+
+			int distance = (int)(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+
+			if(distance > Math.pow(r1+r2, 2)){
+				System.out.println(0);
 			}
-			
-			System.out.println("-----------------------");
-			
-			for(int i=0; i<r1; i++) {
-				int ny = y1-r1+i;
-				int nx = x1+i;
-				System.out.println(ny+","+nx);
+			else if(distance == Math.pow(r1+r2, 2)){
+				System.out.println(1);
 			}
-//			for(int i=1; i<r1; i++) {
-//				int ny = y1+r1-i;
-//				int nx = x1+i;
-//				System.out.println(ny+","+nx);
-//			}
-			System.out.println(cnt);
+			else if(distance < Math.pow(r1-r2, 2)){
+				System.out.println(0);
+			}
+			else if(distance == Math.pow(r1-r2, 2)){
+				System.out.println(1);
+			} else {
+				System.out.println(2);
+			}
 		}
 	}
 }
